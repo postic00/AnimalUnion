71d9b655afe38656e9e27d62a94f9bfedb14e0fa
postic00 <postic00@gmail.com>
2026-03-14 03:49:29 +0900

    modify vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build {
+  build: {
     rollupOptions: {
       external: ['@apps-in-toss/web-framework'],
     },

36463c8372816660105e4e43c6c81982a96f1fa1
postic00 <postic00@gmail.com>
2026-03-14 04:15:05 +0900

    update vite

---
+++ src/stubs/apps-in-toss-web-framework.d.ts
@@ -0,0 +1,6 @@
+declare module '@apps-in-toss/web-framework' {
+	export const loadFullScreenAd: any
+	export const showFullScreenAd: any
+	export const closeView: any
+	export const graniteEvent: any
+}
